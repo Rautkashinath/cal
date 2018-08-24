422da7addb88fe25785a794a1b2edfb99b986c42
Rautkashinath <ganeshraut302@gmail.com>
2018-08-24 21:56:25 +0530

    Add files via upload

---
+++ Upsert.sql
@@ -0,0 +1,23 @@
+
+	
+CREATE PROCEDURE InsertUpdate
+--define Parameters for Procedure
+--Here we are getting User Name and User_id As a Input and update/insert record based on User_id
+@Id  int,
+@Name VARCHAR(50)		
+
+AS
+
+  BEGIN
+
+	  -- SET NOCOUNT ON to prevent extra messages Showing Number of affected rows
+      SET NOCOUNT ON;
+
+      IF EXISTS (SELECT 1 FROM  Users  WHERE  User_Id = @Id)				--check if User with Id already exist
+        UPDATE Users													
+        SET Name = @Name WHERE User_Id=@Id
+
+      ELSE
+        INSERT INTO Users([User_Id],  [Name]) VALUES(@Id, @Name)
+
+  END
